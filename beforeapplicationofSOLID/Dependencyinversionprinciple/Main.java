074aafb3814cd7be94275fb6ff673744fd16955f
lettycoding <laetitia.maffo21@facsciences-uy1.com>
2025-10-29 19:36:48 +0100

    implementation of a source code that doesnot respect the dependency inversion principle

---
+++ beforeapplicationofSOLID/Dependencyinversionprinciple/Main.java
@@ -0,0 +1,8 @@
+package beforeapplicationofSOLID.Dependencyinversionprinciple;
+
+public class Main {
+    public static void main(String[] args) {
+OrderProcessor order = new OrderProcessor();
+order.processOrder("'Données à sauvegarder'");
+}
+}
---
+++ beforeapplicationofSOLID/Dependencyinversionprinciple/OrderProcessor.java
@@ -0,0 +1,16 @@
+package beforeapplicationofSOLID.Dependencyinversionprinciple;
+
+class MySQLDatabase {
+    public void save(String data) {
+System.out.println("Saving to MySQL: " + data);
+
+}
+}
+public class OrderProcessor {
+private MySQLDatabase database;
+public OrderProcessor() {
+this.database = new MySQLDatabase();
+}
+public void processOrder(String order) {
+database.save(order);
+}}
